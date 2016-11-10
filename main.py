91757f8872af3c3cd8a5646d53e279fde2cba897
Robert Gerdisch <rgerdisch@gmail.com>
2016-11-09 20:23:58 -0500

    Added simple routing

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import os.path
+
 from flask import Flask
 app = Flask(__name__)
 
+def readFile(fileName):
+    filePtr = open(fileName, 'r')
+    contents = filePtr.read()
+    return contents
+
 @app.route("/")
-def hello():
-    return "Hello Shanelle!"
+def showIndex():
+    return showPage("index");
+
+@app.route("/<pageName>")
+def showPage(pageName):
+    if(os.path.isfile(pageName)):
+        return readFile(pageName)
+    return getCode(pageName, "index.html");
+
+@app.route("/web/<pageName>/<fileName>")
+def getCode(pageName, fileName):
+    return readFile("web/" + pageName + "/" + fileName)
 
 if __name__ == "__main__":
-    app.run()
+  app.run()

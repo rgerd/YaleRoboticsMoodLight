a30f3960147e0a833132684c290f8e85148a57d6
Robert Gerdisch <rgerdisch@gmail.com>
2017-01-07 19:51:46 -0500

    Fixed

--- util/twitter.py
+++ util/twitter.py
@@ -1,25 +1,24 @@
-import json
 import sys
-from config.twitterconfig import twitter_api
+import json
 
 # Returns a an array of the combined posts by the users in the request (`dict`),
 # which are each filtered by the keywords in `dict`
 # i.e.
-# dict = {
-#   'nachos': ['theRealDonald'],
+# request = {
+#   'nachos': ['realDonaldTrump'],
 #   'burritos': ['HillaryClinton', 'BarackObama'],
 #   'enchaladas': ['HillaryClinton'],
 #   'cows': []
 # }
-# getTwitterData(dict) returns:
+# getTwitterData(request) returns:
 # all tweets by donald trump that contain the word 'nachos'
 # + all tweets by barack obama containing the word 'burritos'
 # + all tweets by hillary clinton containing the word 'burritos'
 # + all tweets by hillary clinton containing the word 'enchaladas'
 # + all tweets by all users containing the word 'cows'
-def getTwitterData(dict):
+def getTwitterData(request):
     # YOUR CODE HERE
-    return [ ]
+    return []
 
 
 ###############################################################################
@@ -29,6 +28,7 @@ def getTwitterData(dict):
 # Example:
 #   getPostsByUser("HillaryClinton", "-Hillary")
 def getPostsByUser(twitter_handle, keyword):
+    from config.twitterconfig import twitter_api
     timeline = twitter_api.GetUserTimeline(screen_name=twitter_handle, count=sys.maxsize)
     tweets = [s.text for s in timeline]
     return filterTweetsByKeyword(tweets, keyword)
@@ -40,4 +40,5 @@ def filterTweetsByKeyword(tweets, keyword):
 # Returns an array of tweets (strings) from all users, each containing the
 # `keyword` specified
 def getPostsByAll(keyword):
+    from config.twitterconfig import twitter_api
     return twitter_api.GetSearch(keyword, count=sys.maxsize, lang="en")

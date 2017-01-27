139e2f2d2d9813e0cd6bebed1f2e2f8e3963500a
Robert Gerdisch <rgerdisch@gmail.com>
2017-01-27 18:36:23 -0500

    Double equals to check for equality

--- util/color.py
+++ util/color.py
@@ -9,18 +9,17 @@
 # Try to use both the label (pos/neg/neutral) as well as the magnitude to pick
 # a color. Inspiration: http://www.w3schools.com/colors/colors_names.asp
 def emotionToColor(emotion):
-    
+
     #yellow = joy
-    if emotion[0] = 'pos':
+    if emotion[0] == 'pos':
         color=60 #yellow
 
     #green=disgust
-    if emotion[0] = 'negative':
+    if emotion[0] == 'negative':
         color=120 #green
-        
+
     else:
         color=0 #neutral or anything else: color will be white
 
     import colorsys
     rgbval = colorsys.hls_to_rgb(color/360, emotion[1], 1.0)
-

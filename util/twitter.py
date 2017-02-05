8bea7c337e457812576c00b0ec9929488b5313d6
Robert Gerdisch <rgerdisch@gmail.com>
2017-02-05 17:04:05 -0500

    Removed merge stuff

--- util/twitter.py
+++ util/twitter.py
@@ -21,7 +21,6 @@ import json
 # + all tweets by donald trump
 
 def getTwitterData(request):
-<<<<<<< Updated upstream
 	all_tweets = []
 	for keyword, users in request.items:
 		if not users:
@@ -30,16 +29,6 @@ def getTwitterData(request):
 			for user in users:
 				all_tweets.extend(getPostsByUser(user, keyword))
 	return all_tweets
-=======
-    all_tweets = []
-    for keyword, users in request.items:
-        if not users:
-            all_tweets.extend(getPostsByAll(keyword))
-        else:
-            for user in users:
-                all_tweets.extend(getPostsByUser(user, keyword))
-    return all_tweets
->>>>>>> Stashed changes
 
 
 ###############################################################################

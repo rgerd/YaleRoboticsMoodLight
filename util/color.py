ead45b408acd97778acbe61c1b5d7b9590a7f2b0
klaire.tan@yale.edu <klaire.tan@yale.edu>
2017-02-05 17:32:25 -0500

    lightness fxn mapping x-values(0 to 1) to (1 to .5)

--- util/color.py
+++ util/color.py
@@ -12,14 +12,18 @@ def emotionToColor(emotion):
 
     #yellow = joy
     if emotion[0] == 'pos':
-        color=60 #yellow
+        color=0 #red
 
     #green=disgust
     if emotion[0] == 'negative':
-        color=120 #green
+        color=225 #blue
 
     else:
-        color=0 #neutral or anything else: color will be white
+        color=60 #neutral or anything else: color will be yellow
+
+    # 0 < mag < 1
+    # 1 < lightness < .5
+    lightness = 1 - .5(emotion[1]);
 
     import colorsys
-    rgbval = colorsys.hls_to_rgb(color/360, emotion[1], 1.0)
+    rgbval = colorsys.hls_to_rgb(color/360, lightness, 1.0)
